87eb91b133811384a16bb9e3dfb5611bd0175f53
Mudit Jain <muditjai@gmail.com>
2016-07-15 15:27:56 -0700

    Refactor code.

--- Code/CreateInputData.py
+++ Code/CreateInputData.py
@@ -1,18 +1,24 @@
+"""
+Create training data with header. Transpose the original gene rows.
+"""
+
 import numpy as np
 import os
 import sys
 from timeit import default_timer as timer
 
 if __name__ == '__main__':
+    inp_file, out_file = ("../training.csv", "../training.transposed.txt")
+
     os.chdir(os.path.dirname(sys.argv[0]))
     start = timer()
-    inp_matrix = np.loadtxt("../training.csv", delimiter=',')
+    inp_matrix = np.loadtxt(inp_file, delimiter=',')
     end = timer()
     print("Read done in {}".format(end - start))
 
     header1 = ",".join(["Gene_{}".format(i+1) for i in range(inp_matrix.shape[0])])
 
     start = timer()
-    np.savetxt("../training.transposed.txt", inp_matrix.transpose(), delimiter=",", fmt="%.2f", header=header1)
+    np.savetxt(out_file, inp_matrix.transpose(), delimiter=",", fmt="%.2f", header=header1)
     end = timer()
     print("Write done in {}".format(end - start))
